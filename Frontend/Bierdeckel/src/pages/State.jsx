2e842906f960503e45b506d95cf6c96b050db071
Matthias Schneider <63250128+Voxel07@users.noreply.github.com>
2024-03-19 08:32:21 +0100

    add StateView

---
+++ Frontend/Bierdeckel/src/pages/State.jsx
@@ -0,0 +1,11 @@
+// Tabs with Tables
+import StateTableOrderd from '../components/state/StateTableOrdered'
+import StateTableInPreperation from '../components/state/StateTableInPreperation'
+import StateTableCompleted from '../components/state/StateTableCompleted'
+
+export default function State()
+{
+    return(
+        <div>State :D</div>
+    )
+}
