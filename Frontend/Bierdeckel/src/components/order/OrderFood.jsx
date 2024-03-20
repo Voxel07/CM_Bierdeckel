ec1682a67e5b02856ebe7e734f5510440dff9a7d
Voxel07 <voxel@vivaldi.net>
2024-03-21 00:48:44 +0100

    test prettier

--- Frontend/Bierdeckel/src/components/order/OrderFood.jsx
+++ Frontend/Bierdeckel/src/components/order/OrderFood.jsx
@@ -1,39 +1,49 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
-import InfoCard from './InfoCard';
-import {
-    Grid,
-    Divider,
-    Chip
-  } from '@mui/material';
+import InfoCard from "./InfoCard";
+import { Grid, Divider, Chip } from "@mui/material";
 
-import './OrderStyle.css';
+import "./OrderStyle.css";
 
-function OrderFood({ products, newItems, handleStockChange, displayITems, handleDisplayItemChange }) {
-    
+function OrderFood({
+  products,
+  newItems,
+  handleStockChange,
+  displayITems,
+  handleDisplayItemChange,
+}) {
   return (
-    <Grid className='grid-container' container>
-        {
-            products?.length ? products.map(product => <Grid className='grid-item' item xl={3} lg={4} md={6} xs={12}>
-                <InfoCard data = {product} userData={displayITems} handelChange={handleStockChange} />
-                </Grid>) :null
-        }
+    <Grid className="grid-container" container>
+      {products?.length
+        ? products.map((product) => (
+            <Grid className="grid-item" item xl={3} lg={4} md={6} xs={12}>
+              <InfoCard
+                data={product}
+                userData={displayITems}
+                handelChange={handleStockChange}
+              />
+            </Grid>
+          ))
+        : null}
 
-        <Grid item xs={12} >
-            <Divider ariant="inset"  sx={{
-                    "&::before, &::after": {
-                    borderColor: "primary.light",
-                    },
-                }}>
-                <Chip label="Curry" size="big" color='primary' sx={{margin:10}} />
-            </Divider>
-        </Grid>
-         {/* <pre>{JSON.stringify(newItems, null, 2)}</pre> */}
-         {/* <pre>{JSON.stringify(displayITems, null, 2)}</pre> */}
+      <Grid item xs={12}>
+        <Divider
+          ariant="inset"
+          sx={{
+            "&::before, &::after": {
+              borderColor: "primary.light",
+            },
+          }}
+        >
+          <Chip label="Curry" size="big" color="primary" sx={{ margin: 10 }} />
+        </Divider>
+      </Grid>
+      {/* <pre>{JSON.stringify(newItems, null, 2)}</pre> */}
+      {/* <pre>{JSON.stringify(displayITems, null, 2)}</pre> */}
 
-        {/* <Grid className='grid-item' item xl={3} lg={4} md={6} xs={12}>
+      {/* <Grid className='grid-item' item xl={3} lg={4} md={6} xs={12}>
             <InfoCard
                 image="https://picsum.photos/id/10/2500/1667" // Placeholder image
                 title="Bratwurst"
--- Frontend/Bierdeckel/src/components/order/shoppingcard.jsx
+++ Frontend/Bierdeckel/src/components/order/shoppingcard.jsx
@@ -1,79 +1,87 @@
+import React, { useEffect, useState } from "react";
 
-import React, {useEffect, useState} from 'react';
-
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Badge from '@mui/material/Badge';
-import Stack from '@mui/material/Stack';
-import Divider from '@mui/material/Divider';
-import CartItem from './cardItem'
-import { summarizeOrderItems } from './orderUtils'; 
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Badge from "@mui/material/Badge";
+import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
+import CartItem from "./cardItem";
+import { summarizeOrderItems } from "./orderUtils";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  borderradius: '20px',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  borderradius: "20px",
   boxShadow: 24,
   p: 4,
 };
 
-export default function shoppingcard({cardData, handleShoppingCard, newItems}) {
+export default function shoppingcard({
+  cardData,
+  handleShoppingCard,
+  newItems,
+}) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const [parsedProducts, setParsedProducts] = React.useState(null);
 
-  useEffect(()=>
-    {
-      if(cardData != null && cardData?.orderItems?.length > 0)
-      {
-        setParsedProducts(summarizeOrderItems(cardData.orderItems));
-      }
-      else
-      {
-        setParsedProducts(null)
-      }
-    },[cardData])
+  useEffect(() => {
+    if (cardData != null && cardData?.orderItems?.length > 0) {
+      setParsedProducts(summarizeOrderItems(cardData.orderItems));
+    } else {
+      setParsedProducts(null);
+    }
+  }, [cardData]);
 
   return (
     <div>
-        <Button onClick={handleOpen}> 
-            <Stack direction="row" spacing={2}>
-                <Badge color="info" size="small" badgeContent={cardData?.orderItems?.length?? 0}>
-                    <ShoppingCartIcon />
-                </Badge>
-               <Typography>{cardData?.sum ?? 0}€</Typography>
-            </Stack>
-        </Button>
+      <Button onClick={handleOpen}>
+        <Stack direction="row" spacing={2}>
+          <Badge
+            color="info"
+            size="small"
+            badgeContent={cardData?.orderItems?.length ?? 0}
+          >
+            <ShoppingCartIcon />
+          </Badge>
+          <Typography>{cardData?.sum ?? 0}€</Typography>
+        </Stack>
+      </Button>
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style} >
-            <Stack direction="row" spacing={2}  justifyContent="space-between" alignItems="baseline">
-                <Typography id="modal-modal-title" variant="h5" component="h2">
-                    Bestellungsübersicht
-                </Typography>
-                <Typography variant="h5" component="h2">{cardData?.sum ?? 0}€</Typography>
-            </Stack>
-          <Divider sx={{marginBottom:4}}/>
-        {
-          parsedProducts?.length ? parsedProducts.map(product => <CartItem product={product}/>)
-          : null
-        }
-         {/* <pre>{JSON.stringify(products, null, 2)}</pre> */}
-         {/* <pre>{JSON.stringify(cardData, null, 2)}</pre> */}
-          
+        <Box sx={style}>
+          <Stack
+            direction="row"
+            spacing={2}
+            justifyContent="space-between"
+            alignItems="baseline"
+          >
+            <Typography id="modal-modal-title" variant="h5" component="h2">
+              Bestellungsübersicht
+            </Typography>
+            <Typography variant="h5" component="h2">
+              {cardData?.sum ?? 0}€
+            </Typography>
+          </Stack>
+          <Divider sx={{ marginBottom: 4 }} />
+          {parsedProducts?.length
+            ? parsedProducts.map((product) => <CartItem product={product} />)
+            : null}
+          {/* <pre>{JSON.stringify(products, null, 2)}</pre> */}
+          {/* <pre>{JSON.stringify(cardData, null, 2)}</pre> */}
         </Box>
       </Modal>
     </div>
