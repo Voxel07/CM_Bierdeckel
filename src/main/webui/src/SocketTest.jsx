67951d39141f2bbd5222c668d99082eb0033140e
Matthias Schneider <63250128+Voxel07@users.noreply.github.com>
2024-07-16 08:46:50 +0200

    revert keycloakTest

--- src/main/webui/src/SocketTest.jsx
+++ src/main/webui/src/SocketTest.jsx
@@ -1,52 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
-import Keycloak from 'keycloak-js';
+// import Keycloak from 'keycloak-js';
 
-// const ENDPOINT = 'ws://localhost:8080//data-updates/1'
+const ENDPOINT = 'ws://localhost:8443//data-updates/1'
 
-// function SocketTest() {
-//     const [sharedValue, setSharedValue] = useState('');
+function SocketTest() {
+    const [sharedValue, setSharedValue] = useState('');
     
-//     const socket = new WebSocket(ENDPOINT);
+    const socket = new WebSocket(ENDPOINT);
 
-//     useEffect(() => {
+    useEffect(() => {
 
-//         socket.onmessage = (event) => {
-//             setSharedValue(parseInt(event.data, 10)); // Assuming data is numeric
-//         };
-//     }, []);
+        socket.onmessage = (event) => {
+            setSharedValue(parseInt(event.data, 10)); // Assuming data is numeric
+        };
+    }, []);
 
-//     const handleIncrement = () => {
-//         socket.send('increment'); // Send a message to the backend
-//     };
+    const handleIncrement = () => {
+        socket.send('increment'); // Send a message to the backend
+    };
 
-//     const handleDecrement = () => {
-//         socket.send('decrement'); 
-//     };
+    const handleDecrement = () => {
+        socket.send('decrement'); 
+    };
 
-//     return (
-//         <div>
-//             <button onClick={handleDecrement}>-</button> 
-//             <span>The current shared value is: {sharedValue}</span>
-//             <button onClick={handleIncrement}>+</button> 
-//         </div>
-//     );
-// }
-
-// export default SocketTest
-
-async function keycloakTest()
-{
-    const keycloak = new Keycloak({
-        url: 'https://kc.matzeschneider.de',
-        realm: 'CM_Bierdeckel',
-        clientId: 'myapp'
-    });
-
-    try {
-        const authenticated = await keycloak.init();
-        console.log(`User is ${authenticated ? 'authenticated' : 'not authenticated'}`);
-    } catch (error) {
-        console.error('Failed to initialize adapter:', error);
-    }
+    return (
+        <div>
+            <button onClick={handleDecrement}>-</button> 
+            <span>The current shared value is: {sharedValue}</span>
+            <button onClick={handleIncrement}>+</button> 
+        </div>
+    );
 }
+
+export default SocketTest
+
+// async function keycloakTest()
+// {
+//     const keycloak = new Keycloak({
+//         url: 'https://kc.matzeschneider.de',
+//         realm: 'CM_Bierdeckel',
+//         clientId: 'myapp'
+//     });
+
+//     try {
+//         const authenticated = await keycloak.init();
+//         console.log(`User is ${authenticated ? 'authenticated' : 'not authenticated'}`);
+//     } catch (error) {
+//         console.error('Failed to initialize adapter:', error);
+//     }
+// }
